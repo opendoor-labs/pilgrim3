927388e4c8ec9678025711abe6665bf1ecdb453f
Matt Wilson <daicoden@copypastel.com>
2016-10-11 11:55:29 -1000

    Initial test setup.
    bin/test now works.

---
+++ test/python/README.md
@@ -0,0 +1,7 @@
+# Python Tests
+
+To run tests run from the root directory:
+
+```sh
+python -m unittest discover ./test/  "*_test.py"
+```
---
+++ test/python/acceptance_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class CommentTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_foo(self):
+        self.assertTrue(True)
