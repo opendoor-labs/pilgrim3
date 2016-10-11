0deed30f7a54703039ec7a2ea2abaed9ecbe7e30
Matt Wilson <daicoden@github.com>
2016-10-10 14:19:49 -1000

    Enable local development.
    Now you can develop by running local server without having to install, allows you to make python changes easily.

--- pilgrim3/scripts/run.py
+++ pilgrim3/scripts/run.py
@@ -2,6 +2,7 @@ import os
 
 import click
 import pkg_resources
+import sys
 
 from pilgrim3.app import app
 
@@ -15,3 +16,10 @@ def main(host, port, proto_bundle):
     app.config['proto-bundle'] = os.path.abspath(proto_bundle)
     app.run(host=host, port=port, threaded=True)
 
+# run from top level directory via:
+# 'python -m pilgrim3/scripts/run.py [options]
+# this sets the module so the local copy of pilgrim is imported, not the one installed on disk
+if __name__ == "__main__":
+    # Manipulate ARGV to make invocation with extra -m argument work
+    sys.argv.pop(0)
+    main()
