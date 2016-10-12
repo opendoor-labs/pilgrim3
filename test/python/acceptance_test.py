932afe46e1555159beb811d41ab6fc09a9510c39
Matt Wilson <daicoden@copypastel.com>
2016-10-11 16:41:15 -1000

    Boot up server and redirect logs to log directory for tests.

--- test/python/acceptance_test.py
+++ test/python/acceptance_test.py
@@ -6,10 +6,10 @@ from pilgrim3.app import app
 from threading import Thread
 import httplib
 from flask import request
+import logging
 
 FAIL = '\033[91m'
 ENDC = '\033[0m'
-
 
 # OMG there is no signaling of threads in python 2.x - so
 # the only way to tell the flask app to shutdown is this.
@@ -19,30 +19,74 @@ ENDC = '\033[0m'
 @app.route('/shutdown', methods=['GET'])
 def shutdown():
     request.environ.get('werkzeug.server.shutdown')()
-    return "done"
+    return "shut down"
+
+@app.route('/booted', methods=['GET'])
+def booted():
+    return "yes"
 
 class CommentTestCase(unittest.TestCase):
-    def __init__(self, message):
-        self.server = None
-        unittest.TestCase.__init__(self, message)
-
-
-    def setUp(self):
+    @classmethod
+    def configure_server_in_thread(cls, hostname, port):
         # for now compile this by running test/support/proto3/compile
         app.config['proto-bundle'] = os.path.abspath("test/support/build/types.build")
-        self.server = Thread(target=lambda: app.run(host="localhost", port=9151, threaded=True))
-        self.server.daemon = True
-        self.server.start()
-        time.sleep(1)  # todo replace with bootstrap class which curls status URL
+        server = Thread(target=lambda: app.run(host=hostname, port=port, threaded=True))
+        server.daemon = True
+        server.start()
+        return server
 
-    def tearDown(self):
-        conn = httplib.HTTPConnection("localhost", 9151)
-        conn.request("GET", "/shutdown")
+    @classmethod
+    def configure_client(cls, hostname, port):
+        return httplib.HTTPConnection(hostname, port, timeout=5)
 
-        self.server.join(5)
-        if self.server.is_alive():
+    @classmethod
+    def wait_for_boot(cls, client):
+        while True:
+            try:
+                client.request("GET", "/booted")
+                client.getresponse()
+            except httplib.CannotSendRequest:
+                time.sleep(0.1)
+                continue
+            break
+
+    @classmethod
+    def redirect_app_logs(cls, filepath):
+        log_names = ['werkzeug']
+        app_logs = map(lambda logname: logging.getLogger(logname), log_names)
+        file_handler = logging.FileHandler('log/app.test.log', 'w')
+
+        for app_log in app_logs:
+            for hdlr in app_log.handlers[:]:  # remove all old handlers
+                app_log.removeHandler(hdlr)
+
+            app_log.addHandler(file_handler)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.client = cls.configure_client("localhost", 9151)
+        cls.server = cls.configure_server_in_thread("localhost", 9151)
+        cls.wait_for_boot(cls.client)
+        cls.redirect_app_logs("log/app.test.log")
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            cls.client.request("GET", "/shutdown")
+        except httplib.CannotSendRequest:
+            print FAIL + "Error shutting down" + ENDC
+
+        cls.server.join(5)
+        if cls.server.is_alive():
             print FAIL + "Unable to gracefully shutdown server"
             print "Resources may not have been released" + ENDC
 
+    def get(self, url):
+        self.__class__.client.request("GET", url)
+        return self.__class__.client.getresponse().read()
+
     def test_message(self):
-        self.assertTrue(True)
+        self.get("/booted")
+
+    def test_another(self):
+        self.assertTrue(1);
